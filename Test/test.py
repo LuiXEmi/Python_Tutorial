3f9eb703438988c908d1addec3f0053ef5810d6c
Luis Emiliano Orozco <luisemiliano@hotmail.com>
2024-03-21 16:53:02 -0600

    Added test.py
    Github Tutorial

---
+++ Test/test.py
@@ -0,0 +1,39 @@
+"""
+#PRINT
+print("Hola Mundo")
+print('o----')
+print(' ||||')
+print ('*' * 10)
+"""
+
+"""
+#INPUT
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+
+print(name + " likes " + color)
+"""
+
+# TYPE CONVERSION
+"""
+birthYear = input("Birth year: ")  
+# Always string type, must convert to int or float in order to perform an operation
+# age = 2024 - birthYear
+age = 2024 - int(birthYear)
+print(age)
+
+weightPounds = input("What is your weight in pounds?: ")
+weightKilograms = int(weightPounds) / 2.20462
+print(weightKilograms)
+"""
+# STRINGS
+"""
+course = "Python for Beginners"
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:5])
+print(course[:])
+print(course[1:-1])
+"""
